2d3bde6cf99467f2e70b70907ab84325b1b8159a
saptaks <saptak.tech193@gmail.com>
2015-08-05 17:39:01 +0530

    GUI added

--- assignment4/Jdbc.java
+++ assignment4/Jdbc.java
@@ -3,12 +3,22 @@
 	We need to take the name as input and find the corresponding marks from the other table.
 	Read the database details from a cnfig file.
 */
+
 import java.sql.*;
 import java.io.*;
 import java.util.Properties;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Jdbc
 {
+
+   //GUI variables
+   private static JFrame mainFrame;
+   private static JLabel headerLabel;
+   private static JLabel statusLabel;
+   private static JPanel controlPanel;
    // JDBC driver name and database URL
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
    static String DB_URL = "";
@@ -16,9 +26,47 @@ public class Jdbc
    //  Database credentials
    static String USER = "";
    static String PASS = "";
+   static String data = "";
+   static String query = "";
    
    public static void main(String[] args) throws IOException
    {
+
+	   mainFrame = new JFrame("JDBC example");
+           mainFrame.setSize(400,400);
+	   mainFrame.setLayout(new GridLayout(3, 1));
+           mainFrame.addWindowListener(new WindowAdapter() {
+	    public void windowClosing(WindowEvent windowEvent){
+	      System.exit(0);
+	    }        
+           });    
+           headerLabel = new JLabel("", JLabel.CENTER);        
+           statusLabel = new JLabel("",JLabel.CENTER);    
+   
+           statusLabel.setSize(350,100);
+ 
+           controlPanel = new JPanel();
+           controlPanel.setLayout(new FlowLayout());
+
+           mainFrame.add(headerLabel);
+           mainFrame.add(controlPanel);
+           mainFrame.add(statusLabel);
+           mainFrame.setVisible(true); 
+		
+	   headerLabel.setText("Marks"); 
+
+           JLabel  namelabel= new JLabel("User ID: ", JLabel.RIGHT);
+           final JTextField userText = new JTextField(6);
+
+           JButton loginButton = new JButton("Submit");
+           loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {     
+               query = userText.getText();
+//              statusLabel.setText(data);        
+            
+           
+
+
 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
    	   Connection conn = null;
 	   Statement stmt = null;
@@ -45,8 +93,8 @@ public class Jdbc
 		if (input != null) {
 			try {
 				input.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+			} catch (IOException ex) {
+				ex.printStackTrace();
 			}
 		}
 	   }
@@ -54,7 +102,7 @@ public class Jdbc
 	   try{
 	      //STEP 2: Register JDBC driver
 	      Class.forName("com.mysql.jdbc.Driver");
-
+//		GUI();
 	      //STEP 3: Open a connection
 	      System.out.println("Connecting to database...");
 	      conn = DriverManager.getConnection(DB_URL, USER, PASS);
@@ -64,7 +112,7 @@ public class Jdbc
 	      System.out.println("Enter Name to be Searched...");
 	      stmt = conn.createStatement();
 	      
-	      String query = br.readLine();
+//	      String query = br.readLine();
 	      String sql = "SELECT * FROM details WHERE name = '" + query + "'";
 	      ResultSet rs = stmt.executeQuery(sql);
 	      System.out.println("\n\n\t Result:\n\n");
@@ -80,8 +128,10 @@ public class Jdbc
 	         String name = rs.getString("name");
 	         String address = rs.getString("address");
 		 String contact = rs.getString("contact");
-		
+		 
 
+		 data = "Name: " + name + "|| Marks: " + marks;		
+		 statusLabel.setText(data);
         	 //Display values
 	         System.out.print("ID: " + id);
 	         System.out.print(", Age: " + age);
@@ -94,9 +144,9 @@ public class Jdbc
 	   }catch(SQLException se){
 	      //Handle errors for JDBC
 	      se.printStackTrace();
-	   }catch(Exception e){
+	   }catch(Exception ex){
 	      //Handle errors for Class.forName
-	      e.printStackTrace();
+	      ex.printStackTrace();
 	   }finally{
 	      //finally block used to close resources
 	      try{
@@ -112,5 +162,14 @@ public class Jdbc
 	      }//end finally try
 	   }//end try
 	   System.out.println("Goodbye!");
+	}
+           }); 
+	 //  statusLabel.setText(data);
+
+	   controlPanel.add(namelabel);
+           controlPanel.add(userText);
+           controlPanel.add(loginButton);
+           mainFrame.setVisible(true);
+
    }//end main
 }//end JDBCExample
