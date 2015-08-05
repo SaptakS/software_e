0cb2491f1bcf887c4015005ea28d3c39454655b3
saptaks <saptak.tech193@gmail.com>
2015-08-05 16:59:02 +0530

    Config file added

--- assignment4/Jdbc.java
+++ assignment4/Jdbc.java
@@ -1,21 +1,51 @@
 import java.sql.*;
 import java.io.*;
+import java.util.Properties;
 
 public class Jdbc
 {
    // JDBC driver name and database URL
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost/db";
+   static String DB_URL = "";
 
    //  Database credentials
-   static final String USER = "root";
-   static final String PASS = "password";
+   static String USER = "";
+   static String PASS = "";
    
    public static void main(String[] args) throws IOException
    {
 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
    	   Connection conn = null;
 	   Statement stmt = null;
+	   Statement stmt1 = null;
+
+	   Properties prop = new Properties();
+           InputStream input = null;
+
+	   try {
+
+		input = new FileInputStream("config.properties");
+
+		// load a properties file
+		prop.load(input);
+
+		// get the property value and print it out
+		DB_URL = prop.getProperty("database");
+		USER = prop.getProperty("dbuser");
+		PASS = prop.getProperty("dbpassword");
+
+	   } catch (IOException ex) {
+		ex.printStackTrace();
+	   } finally {
+		if (input != null) {
+			try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	   }
+	
 	   try{
 	      //STEP 2: Register JDBC driver
 	      Class.forName("com.mysql.jdbc.Driver");
@@ -34,16 +64,23 @@ public class Jdbc
 	      ResultSet rs = stmt.executeQuery(sql);
 	      System.out.println("\n\n\t Result:\n\n");
 	      while(rs.next()){
+		 int id  = rs.getInt("user_id");
+		 stmt1 = conn.createStatement();
+		 String sql1 = "SELECT * FROM marks WHERE id = 1";
+		 ResultSet rs1 = stmt1.executeQuery(sql1);
 	         //Retrieve by column name
-        	 int id  = rs.getInt("user_id");
+		 rs1.next();
+		 int marks = rs1.getInt("marks");
 	         int age = rs.getInt("age");
 	         String name = rs.getString("name");
 	         String address = rs.getString("address");
 		 String contact = rs.getString("contact");
+		
 
         	 //Display values
 	         System.out.print("ID: " + id);
 	         System.out.print(", Age: " + age);
+		 System.out.print(", Marks: " + marks);
 	         System.out.print(", Name: " + name);
 		 System.out.print(", Address: " + address);
 	         System.out.println(", Contact: " + contact);
